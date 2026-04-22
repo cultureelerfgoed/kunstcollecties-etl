73f5b97c91850dd594dc57f05fbc6d4748b36812
Jonathan Blok <guanothan@gmail.com>
2026-04-22 23:06:47 +0200

    debugging slowness

--- src/adlib_harvester.py
+++ src/adlib_harvester.py
@@ -2,10 +2,10 @@
 import urllib.request
 import os
 import logging
-import datetime
 from rdflib import Graph
-import lxml.etree as etree
+import xml.etree.ElementTree as ET
 import adlib_transformer
+import adlib_xpaths as xpath
 
 logger = logging.getLogger(__name__)
 apiLimit = 1000
@@ -14,18 +14,17 @@ GRAPH_ID = os.getenv('GRAPH_ID', 'default')
 OUTPUT_FILE_FORMAT = os.getenv('OUTPUT_FILE_FORMAT', 'json-ld')
 ARTIFACT_PATH = os.getenv('ARTIFACT_PATH', 'kc.jsonld')
 ENCODING = os.getenv('ENCODING', 'utf-8')
-LIMIT = int(os.getenv('LIMIT',1000)) # should be divisible by apiLimit
+LIMIT = int(os.getenv('LIMIT', 141058)) # should be divisible by apiLimit
 
 def harvest(endpoint: str, database='collect', search='all', xmltype='grouped', test=False) -> Graph:
     ## initialize variables for loop
     page = 0
-    numberFound = 1000000
     rgraph = adlib_transformer.get_organization()
     stats = {}
-    date = datetime.datetime.strptime('1900-01-01', '%Y-%m-%d')
+    r_iter = 0
 
     # iterate through resultpages
-    while (numberFound > (page * apiLimit)):
+    while (LIMIT > 0 and r_iter < LIMIT):
         startfrom = page * apiLimit
         # read page of records
         requestUrl = endpoint + \
@@ -37,43 +36,24 @@ def harvest(endpoint: str, database='collect', search='all', xmltype='grouped', 
 
         result = urllib.request.urlopen(requestUrl)
         adlibXML = result.read()
-
-        # parse adlibXML
-        dom = etree.fromstring(adlibXML)
+        root = ET.fromstring(adlibXML)
 
         # get detailed information with priref
-        for record in dom.findall('.//record'):
-            priref = record.get('priref')
-            modification = record.get('modification')
-            mod = datetime.datetime.strptime(modification, '%Y-%m-%dT%H:%M:%S')
-            if mod > date:
-                requestUrl = endpoint + \
-                "?database=" + database + \
-                "&search=priref=" + priref + \
-                "&XMLtype=" + xmltype
-
-                result = urllib.request.urlopen(requestUrl)
-                adlibXML = result.read()
-
+        r_list = root.findall('.//record')
+        if len(r_list) > 0:
+            for record in r_list:
                 # parse adlibXML
-                dom2 = etree.fromstring(adlibXML)
-                adlibXML = etree.tostring(dom2, pretty_print=True)
-
                 try:
-                    rgraph = rgraph +  adlib_transformer.parse_string_to_graph(adlibXML)
+                    rgraph = rgraph +  adlib_transformer.parse_tree_to_graph(record)
+                    r_iter = r_iter + 1
                     stats = adlib_transformer.combine_stats(stats, adlib_transformer.make_statistics_from_string(adlibXML))
                 except TypeError as te:
                     logger.warning('Failed to find an element: %s', te)
-
-        # make loop end
-        ## read numberFound
-        hits = dom.find(".//hits")
-        numberFound = int(hits.text)
-        if int(LIMIT) > 0:
-            numberFound = LIMIT
+        else:
+            break
 
         page = page + 1
-        logger.info('Harvested %s out of %s records', str(page * apiLimit) + '-' + str(page * apiLimit + apiLimit), str(numberFound))
+        logger.info('Harvested %s out of %s records', str(page * apiLimit) + '-' + str(page * apiLimit + apiLimit), str(LIMIT))
 
     adlib_transformer.print_stats(stats)
     return rgraph
